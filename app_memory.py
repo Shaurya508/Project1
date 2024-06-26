21a1086fa6d6c23e7f07af8fd5b87bd60cc1a870
Shaurya508 <shaurya@arymalabs.com>
2024-06-26 16:57:38 +0530

    Update app_memory.py

--- app_memory.py
+++ app_memory.py
@@ -137,24 +137,16 @@ def create_ui():
                 st.session_state.generate_response = False
 
                 js = f"""
-            <script>
-                function scroll(dummy_var_to_force_repeat_execution){{
-                var textAreas = parent.document.querySelectorAll('section.main');
-                for (let index = 0; index < textAreas.length; index++) {{
-                textAreas[index].style.color = '#000000'
-                let scrollInterval = setInterval(() => {{
-                if (textArea.scrollTop < textArea.scrollHeight) {{
-                    textArea.scrollBy(0, 1); // Adjust the second parameter to control the scroll speed
-                }} else {{
-                    clearInterval(scrollInterval);
-                }}
-            }}, 10);
-                textAreas[index].scrollTop = textAreas[index].scrollHeight;
-                }}
-                }}
-                scroll({len(st.session_state.chat)})
-            </script>
-                    """
+<script>
+    function scroll(dummy_var_to_force_repeat_execution){{
+        var textAreas = parent.document.querySelectorAll('section.main');
+        for (let index = 0; index < textAreas.length; index++) {{
+            textAreas[index].scrollTop = textAreas[index].scrollHeight;
+        }}
+    }}
+    scroll({len(st.session_state.chat)})
+</script>
+"""
 
                 st.components.v1.html(js)
                 st.experimental_rerun()
