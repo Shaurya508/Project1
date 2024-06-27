de1cbb427580fde2fa2549f12db9f67f822e1cf6
Shaurya508 <shaurya@arymalabs.com>
2024-06-27 15:38:53 +0530

    Update app_memory.py

--- app_memory.py
+++ app_memory.py
@@ -1,4 +1,74 @@
-ut("Email")
+import streamlit as st
+import pandas as pd
+from memory import user_input
+# Define the maximum number of free queries
+QUERY_LIMIT = 100
+
+# Initialize session state for tracking the number of queries, conversation history, suggested questions, and authentication
+if 'query_count' not in st.session_state:
+    st.session_state.query_count = 0
+
+if 'conversation_history' not in st.session_state:
+    st.session_state.conversation_history = []
+
+if 'suggested_question' not in st.session_state:
+    st.session_state.suggested_question = ""
+
+if 'authenticated' not in st.session_state:
+    st.session_state.authenticated = False
+
+if 'generate_response' not in st.session_state:
+    st.session_state.generate_response = False
+
+if 'chat' not in st.session_state:
+    st.session_state.chat = ""
+
+
+def authenticate_user(email):
+    # Load the Excel file
+    df = pd.read_excel('user.xlsx')
+    # Convert the input email to lowercase
+    email = email.lower()
+    # Convert the emails in the dataframe to lowercase
+    df['Email'] = df['Email'].str.lower()
+    # Check if the email matches any entry in the file
+    user = df[df['Email'] == email]
+    if not user.empty:
+        return True
+    return False
+
+def create_ui():
+    hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    footer:after {content:''; display:block; position:relative; top:2px; color: transparent; background-color: transparent;}
+    .viewerBadge_container__1QSob {display: none !important;}
+    .stActionButton {display: none !important;}
+    ::-webkit-scrollbar {
+        width: 12px;  /* Keep the width of the scrollbar */
+    }
+    ::-webkit-scrollbar-track {
+        background: #f1f1f1;
+    }
+    ::-webkit-scrollbar-thumb {
+        background: #888;
+    }
+    ::-webkit-scrollbar-thumb:hover {
+        background: #555;
+    }
+    </style>
+    """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: #0adbfc;'><u>Aryma Labs - MMM GPT</u></h2>", unsafe_allow_html=True)
+    st.sidebar.image("Aryma Labs Logo.jpeg")
+    st.sidebar.markdown("<h2 style='color: #08daff;'>Welcome to Aryma Labs</h2>", unsafe_allow_html=True)
+    st.sidebar.write("Ask anything about MMM and get accurate answers.")
+
+    if not st.session_state.authenticated:
+        st.markdown("<h3 style='color: #4682B4;'>Login</h3>", unsafe_allow_html=True)
+        with st.form(key='login_form'):
+            email = st.text_input("Email")
             login_button = st.form_submit_button(label='Login')
 
             if login_button:
